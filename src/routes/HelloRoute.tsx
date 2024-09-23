a8f07e1b39b83c55364d38dfa11ffd49f8d58ea8
Maxime Rochat <rochat.max@outlook.com>
2024-09-23 18:10:53 +0200

    v0

--- src/routes/HelloRoute.tsx
+++ src/routes/HelloRoute.tsx
@@ -4,7 +4,7 @@ import OrganizationSidebar from "../components/OverviewPage/OrganizationSidebar"
 export default function HelloPage() {
     return (
         <div>
-            <h1>Version 0.0.0</h1>
+            <h1>Version 0</h1>
         </div>
     )
 }
