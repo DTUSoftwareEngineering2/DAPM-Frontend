7ee75b6ca2dbb08ccaca1779ed17fffc59212bb2
Márk Gyöngyösi <mrkgyngysi@gmail.com>
2024-10-04 15:18:56 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,39 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./redux/slices";
 
-import { persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import { RouterProvider, createBrowserRouter, createHashRouter } from "react-router-dom";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+} from "react-router-dom";
 import PipelineComposer from "./routes/PipeLineComposer";
 import UserPage from "./routes/OverviewPage";
 import { loadState, saveState } from "./redux/browser-storage";
 import HelloPage from "./routes/HelloRoute";
 // Configure redux-persist
+//test comment
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
 };
 
-const persistedReducer = persistReducer<ReturnType<typeof rootReducer>>(persistConfig, rootReducer);
+const persistedReducer = persistReducer<ReturnType<typeof rootReducer>>(
+  persistConfig,
+  rootReducer
+);
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 const store = configureStore({
   reducer: persistedReducer,
   preloadedState: loadState(),
-})
+});
 
 // here we subscribe to the store changes
 store.subscribe(
@@ -39,16 +47,13 @@ store.subscribe(
 );
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-
-
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <UserPage />,
-
   },
   {
     path: "/pipeline",
@@ -57,7 +62,7 @@ const router = createBrowserRouter([
   {
     path: "/hello",
     element: <HelloPage />,
-  }
+  },
 ]);
 
 export default function App() {
