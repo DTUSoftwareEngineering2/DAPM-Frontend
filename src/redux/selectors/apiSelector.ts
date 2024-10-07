fedc270b734169cec32817abb9063b96918ef1c2
Márk Gyöngyösi <mrkgyngysi@gmail.com>
2024-10-07 21:06:44 +0200

    update test

--- src/redux/selectors/apiSelector.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "../states";
-
-export const getOrganizations = (state: RootState) => state.apiState.organizations
-export const getRepositories = (state: RootState) => state.apiState.repositories
-export const getResources = (state: RootState) => state.apiState.resources
