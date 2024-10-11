84c4aabb0387da4d1eacbe8523e1ef26941f49d9
Maxime Rochat <rochat.max@outlook.com>
2024-10-11 10:22:53 +0200

    fixed overview page pipeline grid not taking whole screen

--- src/routes/OverviewPage.tsx
+++ src/routes/OverviewPage.tsx
@@ -2,10 +2,11 @@ import { Box } from "@mui/material";
 import OrganizationSidebar from "../components/OverviewPage/OrganizationSidebar";
 import PipelineAppBar from "../components/PipeLineComposer/PipelineAppBar";
 import PipelineGrid from "../components/OverviewPage/PipelineGrid";
+import { WidthFull } from "@mui/icons-material";
 
 export default function UserPage() {
   return (
-    <div>
+    <div style={{ width: "100%", height: "100%" }}>
       <Box sx={{ display: "flex" }}>
         <OrganizationSidebar />
         <PipelineGrid />
