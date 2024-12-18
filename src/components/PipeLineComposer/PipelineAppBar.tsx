63cebaa80a6ec5501c30bddad8eb850917b3c7ca
Thomas Corthay <thomas.corthay@proton.me>
2024-12-18 15:07:52 +0100

    First

--- src/components/PipeLineComposer/PipelineAppBar.tsx
+++ src/components/PipeLineComposer/PipelineAppBar.tsx
@@ -148,7 +148,11 @@ export default function PipelineAppBar() {
   };
 
   const flowData = useSelector(getActiveFlowData);
-
+  
+  /**
+  * @author Thomas Corthay (s241749) & Grace Ledin (s241742)
+  * @date 2024-12-04
+  */
   const [executionHistory, setExecutionHistory] = useState<{ timestamp: string; }[]>([]);
 
   const generateJson = async () => {
@@ -296,26 +300,22 @@ export default function PipelineAppBar() {
       requestData
     );
 
+    /**
+    * @author Thomas Corthay (s241749) & Grace Ledin (s241742)
+    * @date 2024-12-04
+    */
     const sendData = await setExecutionDate(
       selectedOrg.id,
       selectedRepo.id,
       pipelineId,
       new Date().toISOString()
     );
-
-    const dateListStrin = await getExecutionDate(
-      selectedOrg.id,
-      selectedRepo.id,
-      pipelineId
-    );
-
+    
     const dateListString = await getExecutionDate(
       selectedOrg.id,
       selectedRepo.id,
       pipelineId
     );
-
-    console.log(dateListString)
 
     // Parsing the dateListString into an array
     let dateList = dateListString
@@ -324,6 +324,8 @@ export default function PipelineAppBar() {
       .map((date: string) => date.replace(/"/g, '').trim());
 
     setExecutionHistory(dateList.map((date: string) => ({ timestamp: date })));
+    
+    // --------- end of Thomas' Corthay & Grace's Ledin part-----------
 
     const executionId = await putExecution(
       selectedOrg.id,
@@ -386,6 +388,10 @@ export default function PipelineAppBar() {
     });
   };
 
+  /**
+  * @author Thomas Corthay (s241749)
+  * @date 2024-10-11
+  */
   const [user, setUser] = useState<User | null>(null);
 
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
@@ -395,6 +401,7 @@ export default function PipelineAppBar() {
       getUserInfo(auth.accessToken).then(userInfo => setUser(userInfo));
     }
   }, []);
+  // --------- end of Thomas' Corthay part-----------
 
   return (
     <AppBar position="fixed">
@@ -428,6 +435,13 @@ export default function PipelineAppBar() {
             </Box>
           )}
         </Box>
+
+        {/**
+         * @author Thomas Corthay (s241749)
+         * @date 2024-10-11
+         * @description Displays buttons for toggling a table, viewing outputs, and showing user initials if logged in.
+         */
+        }
         < Box sx={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
           {/* <Button onClick={toggleTable} sx={{ marginRight: '20px' }}>
             <Typography variant="body1" sx={{ color: "white" }}>Show Status</Typography>
@@ -494,7 +508,13 @@ export default function PipelineAppBar() {
         message="Changes saved."
       />
 
-      {/* Execution History Dialog */}
+      {/**
+       * @author Thomas Corthay (s241749) & Grace Ledin (s241742)
+       * @date 2024-12-16
+       * @description Displays an execution history dialog with a table showing timestamps. 
+       * Provides a close button and handles empty history gracefully.
+       */
+      }
       <Dialog
         open={executionHistoryOpen}
         onClose={() => setExecutionHistoryOpen(false)}
@@ -622,6 +642,13 @@ export default function PipelineAppBar() {
         </Box>
       </Modal >
 
+      {
+        /** 
+         * @author Thomas Corthay (s241749)
+         * @date 2024-10-11
+         * @description Display detailed user information with actions to log out or close.
+        */
+      }
       {
         selectedUser && (
           <Box
